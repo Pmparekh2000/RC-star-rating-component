7e0641916b1482323e6203702f35a685ee72ca9e
pmparekh <prerak.parekh@nielsen.com>
2025-11-04 05:38:12 +0530

    Completed star rating component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import StarRating from "./components/StarRating";
 
 function App() {
+  const [currentRating, setCurrentRating] = useState(3);
+  const handleRatingChange = (newRating) => {
+    setCurrentRating(newRating);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Star Rating</h2>
+      <StarRating
+        size={5}
+        rating={currentRating}
+        onChange={handleRatingChange}
+      />
+      <p>Current Rating : {currentRating}</p>
     </div>
   );
 }
